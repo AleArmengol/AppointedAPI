c4c01569499866da7381aa188c5c3905ca9a64fa
Alexis Armengol <aearmengol@gmail.com>
2020-05-11 09:31:01 -0300

    Untrack files in .gitignore

--- db/migrate/20200510213820_create_doctor_calendars.rb
+++ db/migrate/20200510213820_create_doctor_calendars.rb
@@ -1,6 +1,7 @@
 class CreateDoctorCalendars < ActiveRecord::Migration[6.0]
   def change
     create_table :doctor_calendars do |t|
+      t.references :doctor, null: false, foreign_key: true
       t.string :speciality_name
       t.date :start_time
       t.date :end_time
