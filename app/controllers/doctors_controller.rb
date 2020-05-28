2408e9d9f772f676b95d459bbd5fc0a0fc1f3016
tomasmini <minissaletomas@gmail.com>
2020-05-28 10:33:05 -0300

    Cancel appointments methos added

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,51 +1,59 @@
 class DoctorsController < ApplicationController
-  before_action :set_doctor, only: [:show, :update, :destroy]
+  # before_action :set_doctor, only: [:show, :update, :destroy]
 
   # GET /doctors
   def index
-    @doctors = Doctor.all
+    # doctor_specialities = DoctorSpeciality.find(params[:speciality_id])
+    # doctors_specialities_id = doctor_specialities.doctor_id
+    # doctors_found = Doctor.find(doctors_specialities_id.doctor_id)
+    # doctors_names = doctors_found.last_name
+    ds = DoctorSpeciality.where(speciality_id: params[:speciality_id])
+    did = ds.select(:doctor_id)
+    doctors = Doctor.where(id: did)
+    
+    render json: doctors
 
-    render json: @doctors
+    # render json: @doctors
   end
 
   # GET /doctors/1
-  def show
-    render json: @doctor
-  end
+  # def show
+  #   render json: @doctor
+  # end
 
   # POST /doctors
-  def create
-    @doctor = Doctor.new(doctor_params)
+  # def create
+  #   @doctor = Doctor.new(doctor_params)
 
-    if @doctor.save
-      render json: @doctor, status: :created, location: @doctor
-    else
-      render json: @doctor.errors, status: :unprocessable_entity
-    end
-  end
+  #   if @doctor.save
+  #     render json: @doctor, status: :created, location: @doctor
+  #   else
+  #     render json: @doctor.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # PATCH/PUT /doctors/1
-  def update
-    if @doctor.update(doctor_params)
-      render json: @doctor
-    else
-      render json: @doctor.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @doctor.update(doctor_params)
+  #     render json: @doctor
+  #   else
+  #     render json: @doctor.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /doctors/1
-  def destroy
-    @doctor.destroy
-  end
+  # def destroy
+  #   @doctor.destroy
+  # end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_doctor
-      @doctor = Doctor.find(params[:id])
-    end
+  # private
+    # # Use callbacks to share common setup or constraints between actions.
+    # def set_doctor
+    #   @doctor = Doctor.find(params[:id])
+    # end
 
     # Only allow a trusted parameter "white list" through.
-    def doctor_params
-      params.require(:doctor).permit(:name, :last_name, :phone_number, :email, :address)
-    end
+    # def doctor_params
+    #   params.require(:doctor).permit(:name, :last_name, :phone_number, :email, :address)
+    # end
 end
