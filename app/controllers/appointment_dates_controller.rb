5e210a0f732535277b48cd5f4447ad058ed223e9
Alexis Armengol <aearmengol@gmail.com>
2020-05-30 01:05:08 -0300

    Now AppointmentDatesController only returns dates after the current date

--- app/controllers/appointment_dates_controller.rb
+++ app/controllers/appointment_dates_controller.rb
@@ -1,6 +1,6 @@
 class AppointmentDatesController < ApplicationController
   def index
-    dates = Appointment.where(speciality_name: params[:speciality_name]).available.pluck(:start_time).map(&:to_date).uniq
+    dates = Appointment.where(speciality_name: params[:speciality_name]).where('start_time > ?', Date.today).available.pluck(:start_time).map(&:to_date).uniq
 
     @appointment_dates = dates.map do |date|
         AppointmentDate.new(date)
